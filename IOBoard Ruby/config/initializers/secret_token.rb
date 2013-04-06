6e5d89dbb53c17fb2e145b61712bfad83cad630f
Patrick Cuming, Jr. <patrickcumingjr@gmail.com>
2013-04-06 03:23:11 -0500

    initial rails project creation files

---
+++ IOBoard Ruby/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IOBoardRuby::Application.config.secret_token = '1e1c44b268a7d606d5ff74576eee0745ae3aac9d09276868b25bcd7b9523f586951cc9021e9370d61ee1e1d299f8074f4500e88ae0fe038f8b46e7e723e65c2c'
